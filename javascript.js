67e265274d71bd72a4b744cbf5ed2d99bb4b5c26
Karinne Bruno <karinnebruno@MacBook-Air-de-Karinne.local>
2025-09-16 09:40:08 -0300

    java

---
+++ javascript.js
@@ -0,0 +1,8 @@
+//Trabalhando com saída de dados.
+console.log("Fui criado no arquivo Javascript");
+//Tipos de dados primitivos
+console.log('Um texto aqui');//string
+console.log('29');//number
+console.log(true,false);//boolean
+console.log(null); //null
+console.log(undefined); //undefined
